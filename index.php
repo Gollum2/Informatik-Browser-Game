5da59c697d13bcfdc4872f2d8f2156d1ff96d594
simom <simomgraffonara@gmail.com>
2022-02-19 21:18:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <title>Browser game</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+
+<body style="background-color:gray">
+    <?php
+    function generateRandomString($length = 25)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=?';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    session_start();
+    $usernamestring = "";
+    $passwordstring = "";
+    $hidden1 = "hidden";
+    $hidden2 = "";
+    $errormessage = "";
+    if (isset($_SESSION["user"])) {
+
+        $usernamestring = $_SESSION["user"];
+        $passwordstring = $_SESSION["pass"];
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (isset($_POST["newusername"], $_POST["newpassword"])) {
+            $servername = "localhost";
+            $usernamedb = "root";
+            $passworddb = "admin";
+            $dbname = "infoprojekt";
+            // Create connection
+            $conn = new mysqli($servername, $usernamedb, $passworddb, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $stmt = $conn->prepare("SELECT * FROM userr where username=?");
+            $stmt->bind_param("s", $_POST["newusername"]);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if (count($result->fetch_all()) == 0) {
+                $stmt = $conn->prepare('Insert into userr (username,passwort) values (?,?);');
+                $stmt->bind_param("ss", $_POST["newusername"], $_POST["newpassword"]);
+                $stmt->execute();
+            }
+        }
+        if (isset($_POST["register"])) {
+            $hidden1 = "";
+            $hidden2 = "hidden";
+        }
+        if (isset($_POST["userpostname"]) and isset($_POST["userpassword"])) {
+            $username = $_POST["userpostname"];
+            $pass = $_POST["userpassword"];
+            //hier überprüfen ob user gültig ist oder nicht
+            $servername = "localhost";
+            $usernamedb = "root";
+            $passworddb = "admin";
+            $dbname = "infoprojekt";
+            // Create connection
+            $conn = new mysqli($servername, $usernamedb, $passworddb, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $stmt = $conn->prepare("SELECT * FROM userr where username=?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $erg = $result->fetch_all();
+            $erg = $erg[0];
+            $conn->close();
+            //$randomString = generateRandomString(40);
+            $_SESSION["pass"] = $pass;
+            $_SESSION["user"] = $username;
+            if ($erg[2] == $pass) {
+                $_SESSION["id"] = $erg[0];
+                //print_r($randomString);
+                header("Location: http://localhost:81/game.php?");
+                exit;
+            }
+            $usernamestring = $username;
+            $passwordstring = $pass;
+            $errormessage = "Username oder Password incorrect";
+        }
+    }
+    ?>
+    <div class="center" <?php echo $hidden2 ?>>
+        <h1>Login</h1>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label for="un">Username: </label><br><input id="un" name="userpostname" type="text" value="<?php echo $usernamestring ?>" /><br>
+            <label for="psw">Password: </label><br><input id="psw" name="userpassword" type="password" value="<?php echo $passwordstring ?>" /><br>
+            <input type="submit" value="Login" />
+        </form>
+        <p><?php echo $errormessage ?>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input type="hidden" name="register" value="Gollum" />
+            <input type="submit" value="Register" />
+        </form>
+        <br>
+    </div>
+    <div class="center" <?php echo $hidden1 ?>>
+        <h1>Register</h1>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input name="newusername" type="text" value="" placeholder="Username" required /><br>
+            <input id="p2" name="newpassword" type="password" value="" placeholder="Password" oninput="checkifsame()" /><br>
+            <input id="p1" name="newpassword" type="password" value="" placeholder="Confirm Password" oninput="checkifsame()" />
+            <p id="error"></p>
+
+            <input type="submit" />
+        </form>
+        <p><?php echo $errormessage ?>
+        <p id="error"></p>
+
+    </div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script>
+        function checkifsame() {
+            console.log("funny");
+            let a = document.getElementById("p1");
+            let b = document.getElementById("p2");
+            console.log(b.value);
+            console.log(a.value);
+            if (a.value == b.value) {
+                document.getElementById("error").textContent = ""
+                if (a.value.length < 5 || a.value.lenght > 44) {
+                    document.getElementById("error").textContent = "Passwort muss zwischen 5 und 45 zeichen lang sein"
+                }
+            } else {
+                document.getElementById("error").textContent = "The passwords are not equal"
+            }
+
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,152 @@
+console.log("fffffffffffffffffffff");
+class feld {
+    constructor(farbcode) {
+        this.x = 0;
+        this.y = 0;
+        this.fieldcolor = farbcode;
+    }
+    gethtml() {
+        return "<p>feld</p>";
+    }
+    move(xx, yy) {
+        this.x = this.x + xx;
+        this.y = this.y + yy;
+    }
+}
+
+tasten = {};
+document.addEventListener('keydown', function (e) {
+    tasten[e.keyCode || e.which] = true;
+}, true);
+document.addEventListener('keyup', function (e) {
+    tasten[e.keyCode || e.which] = false;
+}, true);
+
+let allefelder = [];
+console.log(allefelder);
+allefelder.push([]);
+console.log(allefelder);
+let width = 0;
+let height = 0;
+let fieldcoutheight = 0;
+let fieldcoutwidth = 0;
+let widthmargin = 0;
+let heightmargin = 0;
+let fieldsize = 100;
+//adapt shown tiles to window size at beginning and on resize event
+width = window.innerWidth;
+height = window.innerHeight;
+console.log(height);
+fieldcoutheight = parseInt((height / fieldsize), 10);
+fieldcoutwidth = parseInt((width / fieldsize), 10);
+heightmargin = height - fieldcoutheight * fieldsize;
+widthmargin = width - fieldcoutwidth * fieldsize;
+console.log(fieldcoutheight);
+console.log(fieldcoutwidth);
+console.log("fehler");
+
+window.addEventListener('resize', function (event) {
+    console.log("resize");
+    var newWidth = window.innerWidth;
+    var newHeight = window.innerHeight;
+    //console.log(newHeight);
+    fieldcoutheight = parseInt((newHeight / fieldsize), 10);
+    fieldcoutwidth = parseInt((newWidth / fieldsize), 10);
+    heightmargin = newHeight - fieldcoutheight * fieldsize;
+    widthmargin = newWidth - fieldcoutwidth * fieldsize;
+    if (fieldcoutheight % 2 == 0) {
+        fieldcoutheight -= 1;
+        heightmargin += fieldsize / 2;
+    }
+    if (fieldcoutwidth % 2 == 0) {
+        fieldcoutwidth -= 1;
+        widthmargin += fieldsize / 2;
+    }
+    console.log(fieldcoutheight+" ist die hohe");
+    console.log(fieldcoutwidth+" ist die breite");
+    //'#'+Math.floor(Math.random()*16777215).toString(16); //random color 
+    let temp = [];
+    for (var i = 0; i < fieldcoutheight + 2; i++) {
+        let temp2 = [];
+        for (var j = 0; j < fieldcoutwidth + 2; j++) {
+            let randoclor = '#' + Math.floor(Math.random() * 16777215).toString(16); //random color
+            let fff = new feld(randoclor)
+            fff.x = j;
+            fff.y = i;
+            temp2.push(fff);
+        }
+        temp.push(temp2);
+    }
+    console.log(temp);
+    /*temp.forEach(element => {
+        console.log(JSON.stringify(element));
+    });*/
+    allefelder = temp;
+    let test = document.getElementById("maindiv");
+    test.innerHTML = "";
+    for (var i = 0; i < ( fieldcoutheight+2); i++) {
+        for (var j = 0; j < (fieldcoutwidth+2); j++) {
+            let temp = document.createElement("div");
+            temp.className = "feld";
+            temp.style.position = "absolute";
+            console.log(i+" -- "+j);
+            //console.log(allefelder[i][j].x * fieldsize);
+            //console.log(allefelder[i][j].y * fieldsize);
+            console.log(JSON.stringify(allefelder[i][j]));
+            temp.style.left = (allefelder[i][j].x * fieldsize) + "px";
+            temp.style.top = (allefelder[i][j].y * fieldsize) + "px";
+            temp.style.backgroundColor = allefelder[i][j].fieldcolor;
+            test.appendChild(temp);
+        }
+    }
+});
+
+
+
+let speed = 5;
+let coordsx = 0;
+let coordsy = 0;
+function gameloop() {
+    console.log("---");
+    let vertikal = 0;
+    let horizonzal = 0;
+    if (tasten[87] == true) {
+        console.log("WWWWWWWWW");
+        vertikal -= speed;
+    }
+    if (tasten[65] == true) {
+        console.log("AAAAAAA");
+        horizonzal -= speed;
+    }
+    if (tasten[83] == true) {
+        console.log("SSSSSSsss");
+        vertikal += speed;
+    }
+    if (tasten[68] == true) {
+        console.log("DDDDDDDD");
+        horizonzal += speed;
+    }
+    coordsx += horizonzal;
+    coordsy += vertikal;
+
+    //let top = parseInt(parseInt(test.style.top.replace("px", "")) + vertikal) + "px"
+    //let left = parseInt(parseInt(test.style.left.replace("px", "")) + horizonzal) + "px";
+
+    let left = $("#maindiv").css("left");
+    let top = $("#maindiv").css("top");
+    console.log(coordsx);
+    console.log(coordsy);
+    $("#maindiv").css.left = coordsx + "px";
+    $("#maindiv").css.top = coordsy + "px";
+    setTimeout(gameloop, 10); //update intervall in ms
+}
+gameloop();
+
+$(".feld").on("mouseover", function () {
+    console.log("mouseonfield");
+    $(this).css("border", "1px solid black")
+});
+$(".feld").on("mouseout", function () {
+    console.log("mouseonfield");
+    $(this).css("border", "none")
+});
