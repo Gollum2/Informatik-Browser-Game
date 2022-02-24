4475708b370100568fff1802821853f0aa0a4324
Simon <60666219+Gollum2@users.noreply.github.com>
2022-02-24 20:54:05 +0100

    Working world generation. need ot adjust noise seittings to create a better world. else working fine. try to display value of each tile

--- game.php
+++ game.php
@@ -36,7 +36,8 @@
         var displaywindow = [];
         let renderdistance = 1;
         var loadedchunks = []; // all chunks that are stored are inside as chunk objekt
-        var masterseed=42;
+        var masterseed = 42;
+        var chunksize = 8;
         class Tile {
             content;
             posx;
@@ -53,23 +54,74 @@
         function setup() {
             c = createCanvas(width, height);
             //console.log(getchunk(42, 0, 0));
-            loadchunk(0,0);
+            //loadchunk(0, 0);
             //noLoop();
+            console.log(tilemap);
             c.position(0, 0);
             stroke(255); // Set line drawing color to white
-            frameRate(30);
+            frameRate(5);
             print("test1");
             adaptsizes(width, height);
-            print("test2");
+            background("#808080");
+            let invchunk = involvedchunks();
+            console.log(invchunk);
+            console.log(invchunk.length);
+            for (let i = 0; i < invchunk.length; i++) {
+                print(i + " i");
+                loadchunk(invchunk[i][0], invchunk[i][1]);
+            }
+            console.log("-------------------------------------");
         }
 
         function draw() {
-            for (let i = 0; i < fieldcountheight; i++) {
-                for (let j = 0; j < fieldcountwidth; j++) {
-                    fill(tilemap[i][j].color);
+            leftborder = xxx - ceil(fieldcountwidth / 2);
+            topborder = yyy - ceil(fieldcountheight / 2);
+            for (let i = -1; i < fieldcountheight + 1; i++) {
+                for (let j = -1; j < fieldcountwidth + 1; j++) {
+                    //console.log((topborder + i)+" "+(leftborder + j));
+                    //console.log(tilemap);
+                    //console.log(tilemap[topborder + i][leftborder + j].color + " color");
+                    fill(tilemap[topborder + i][leftborder + j].color);
+                    //print(j * (fieldsize) + leftofsett);
+                    //fill("#ff00ff")
+                    if (i == floor(fieldcountheight / 2) && j == floor(fieldcountwidth / 2)) {
+                        fill("#ff0000");
+                    }
                     rect(j * (fieldsize) + leftofsett, i * (fieldsize) + topofsett, fieldsize, fieldsize);
+                    textSize(320);
+                    text(tilemap.value,j * (fieldsize) + leftofsett, i * (fieldsize) + topofsett);
                 }
             }
+        }
+
+        Array.prototype.containsArray = function(val) {
+            var hash = {};
+            for (var i = 0; i < this.length; i++) {
+                hash[this[i]] = i;
+            }
+            return hash.hasOwnProperty(val);
+        }
+
+        function involvedchunks() {
+            erg = [];
+            print(xxx + " " + yyy + " " + fieldcountwidth + " " + fieldcountheight + " involvedchungs");
+            leftborder = xxx - ceil(fieldcountwidth / 2);
+            topborder = yyy - ceil(fieldcountheight / 2);
+            rightborder = xxx + ceil(fieldcountwidth / 2);
+            bottomborder = yyy + ceil(fieldcountheight / 2);
+            print(leftborder + " " + rightborder + " " + topborder + " " + bottomborder + " da lfjal");
+            for (i = topborder; i <= bottomborder; i += 5) { //check distanz einstellen 5
+                for (j = leftborder; j <= rightborder; j += 5) {
+                    let cpy = floor(i / chunksize);
+                    let cpx = floor(j / chunksize);
+                    if (erg.containsArray([cpx, cpy]) == false) {
+                        erg.push([cpx, cpy]);
+                    }
+                }
+            }
+
+            return erg;
+
         }
 
         function windowResized() {
@@ -80,7 +132,7 @@
         function mouseWheel(event) {
             print("event.delta");
         }
-        3
+
 
         function adaptsizes(width, height) {
             fieldcountwidth = round(width / fieldsize);
@@ -114,18 +166,18 @@
         }
 
         async function loaddinger(seed, cx, cy) {
-            console.log("very funny");
+            //console.log("very funny");
             url = 'http://localhost:4444/chunk/' + seed + "/" + cx + "/" + cy;
-            console.log(url);
+            //console.log(url);
             const response = await fetch(url);
             myJson = await response.json(); //extract JSON from the http response
-            console.log(myJson.className);
+            //console.log(myJson.className);
             return myJson;
         }
 
         function getchunk(seed, cx, cy) {
             let ttt = null;
-            console.log("ouput is funny");
+            //console.log("ouput is funny");
             var prom = new Promise(function(resolve, reject) {
                 try {
                     resolve(loaddinger(seed, cx, cy));
@@ -137,6 +189,22 @@
                 console.log("chunk " + cx + "/" + cy + " arrived");
                 console.log(result);
                 loadedchunks.push(result);
+                let ccy = 0;
+                for (let i = cy * 8; i < (cy + 1) * 8; i++, ccy++) {
+                    //todo memory usage can be shity because 
+                    //i set array at certain values and it fills al the oters
+                    //console.log("in der for schelife");
+                    let ccx = 0;
+                    //console.log(Array.isArray(tilemap[i]));
+                    if (Array.isArray(tilemap[i]) == false) {
+                        tilemap[i] = [];
+                        print("array hinzugefugt")
+                    }
+                    for (let j = cx * 8; j < (cx + 1) * 8; j++, ccx++) {
+                        //console.log(i+" "+j+" is a known coordinate");
+                        tilemap[i][j] = result.tile[ccy][ccx];
+                    }
+                }
             }, function(err) {
                 console.log(err);
             });
@@ -144,32 +212,57 @@
 
         function loadchunk(cx, cy) {
             let ccc = null;
-            let lol=0;
-            while (ccc == null) {
-                for (let i = 0; i < loadedchunks.length; i++) { //todo optimise by searching backwords because last added chunk will be at he end
-                    if (loadedchunks[i].chunkposx == cx && loadedchunks[i].chunkposy == cy) {
-                        ccc = loadedchunks[i];
-                        break;
-                    }
-                }
-                console.log("in der while");
-                if (ccc == null && lol==0) {
-                    getchunk(masterseed, cx, cy)
-                    lol=1;
-                } 
-            }
-            let ccy=0;
+            let lol = 0;
+            //console.log("tstadfasdfa");
             for (let i = cy * 8; i < (cy + 1) * 8; i++) {
                 //todo memory usage can be shity because 
                 //i set array at certain values and it fills al the oters
+                //console.log("in der for schelife");
+                let ccx = 0;
+                if (Array.isArray(tilemap[i]) == false) {
+                    tilemap[i] = [];
+                    print("array hinzugefugt")
+                }
+                for (let j = cx * 8; j < (cx + 1) * 8; j++, ccx++) {
+                    lol = new Tile();
+                    lol.type = "FogOfWar";
+                    lol.posx = j;
+                    lol.posy = i;
+                    lol.color = "#808080"
+                    tilemap[i][j] = lol;
+                }
+            }
+            for (let i = 0; i < loadedchunks.length; i++) { //todo optimise by searching backwords because last added chunk will be at he end
+                if (loadedchunks[i].chunkposx == cx && loadedchunks[i].chunkposy == cy) {
+                    ccc = loadedchunks[i];
+                    console.log("chunk already stored no need to get it");
+                    break;
+                }
+            }
+            if (ccc == null) {
+                getchunk(masterseed, cx, cy)
+                console.log("chunk not found");
+            }
+            print(tilemap);
+            /*
+            lol=0;
+            while (ccc == null) {
+                console.log("in der while");
+                
+            }
+            let ccy = 0;
+            for (let i = cy * 8; i < (cy + 1) * 8; i++,ccy++) {
+                //todo memory usage can be shity because 
+                //i set array at certain values and it fills al the oters
                 console.log("in der for schelife");
-                let ccx=0;
+                let ccx = 0;
                 tilemap[i] = [];
-                for (let j = cx * 8; j < (cx + 1) * 8; j++,ccx++) {
+                for (let j = cx * 8; j < (cx + 1) * 8; j++, ccx++) {
                     tilemap[i][j] = ccc.tile[ccy][ccy];
                 }
             }
             console.log(tilemap);
+            */
         }
     </script>
     <?php
